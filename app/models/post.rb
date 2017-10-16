3bdd76eef5799636c3a99f8b4a85aa7f9e02fff1
Nik <nerdvestor@gmail.com>
2017-10-15 19:59:40 -0700

    add image sizing and title placement

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	has_attached_file :image, styles: {large: "800x400", medium: "500x175>"}
+	has_attached_file :image, styles: {large: "900x400", medium: "500x175>"}
 	validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
 
