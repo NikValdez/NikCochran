f761b9856a0abc41b937581c0570024eeed7f7e5
Nik <nerdvestor@gmail.com>
2017-10-24 11:40:30 -0700

    fix production scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,8 @@
-$(function() {
-	$(".button").click(function() {
-	    $('html,body').animate({
-	        scrollTop: $(".single-service1").offset().top},
-	        'slow');
-	});
-});
-
 // $(function() {
-//     $('.button').click (function() {
-//       $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-//       return false;
-//     });
-//   });
+// 	$(".button").click(function() {
+// 	    $('html,body').animate({
+// 	        scrollTop: $(".single-service1").offset().top},
+// 	        'slow');
+// 	});
+// });
+
